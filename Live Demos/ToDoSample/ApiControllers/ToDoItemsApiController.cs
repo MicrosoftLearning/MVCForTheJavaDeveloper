cd1b2418ae8c9e81b0acff6d9a5bc9cf28878860
GeekTrainer <charrison@geektrainer.com>
2015-03-11 15:52:30 -0700

    Final upload

---
+++ Live Demos/ToDoSample/ApiControllers/ToDoItemsApiController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using ToDoSample.Models;
+
+namespace ToDoSample.ApiControllers
+{
+    public class ToDoItemsApiController : ApiController
+    {
+        private ToDoDbContext db = new ToDoDbContext();
+
+        // GET: api/ToDoItemsApi
+        public IQueryable<ToDoItem> GetToDoItems()
+        {
+            return db.ToDoItems;
+        }
+
+        // GET: api/ToDoItemsApi/5
+        [ResponseType(typeof(ToDoItem))]
+        public async Task<IHttpActionResult> GetToDoItem(int id)
+        {
+            ToDoItem toDoItem = await db.ToDoItems.FindAsync(id);
+            if (toDoItem == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(toDoItem);
+        }
+
+        // PUT: api/ToDoItemsApi/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutToDoItem(int id, ToDoItem toDoItem)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != toDoItem.ToDoItemID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(toDoItem).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ToDoItemExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/ToDoItemsApi
+        [ResponseType(typeof(ToDoItem))]
+        public async Task<IHttpActionResult> PostToDoItem(ToDoItem toDoItem)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.ToDoItems.Add(toDoItem);
+            await db.SaveChangesAsync();
+
+            return CreatedAtRoute("DefaultApi", new { id = toDoItem.ToDoItemID }, toDoItem);
+        }
+
+        // DELETE: api/ToDoItemsApi/5
+        [ResponseType(typeof(ToDoItem))]
+        public async Task<IHttpActionResult> DeleteToDoItem(int id)
+        {
+            ToDoItem toDoItem = await db.ToDoItems.FindAsync(id);
+            if (toDoItem == null)
+            {
+                return NotFound();
+            }
+
+            db.ToDoItems.Remove(toDoItem);
+            await db.SaveChangesAsync();
+
+            return Ok(toDoItem);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool ToDoItemExists(int id)
+        {
+            return db.ToDoItems.Count(e => e.ToDoItemID == id) > 0;
+        }
+    }
+}
